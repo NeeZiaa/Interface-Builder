1c4f599dbff965a72df984039796ec0e1f630bbd
NeeZiaa <73132970+NeeZiaa@users.noreply.github.com>
2023-09-05 22:27:26 +0200

    Refacto

--- src/utils/countChildren.tsx
+++ src/utils/countChildren.tsx
@@ -2,12 +2,12 @@ import { Children, ReactNode, isValidElement } from "react";
 
 export const filterRecursive = (children: ReactNode, filterFn: (child: ReactNode) => boolean): ReactNode[] => {
     return Children.toArray(children).reduce((acc: ReactNode[], child: ReactNode) => {
-      if (filterFn(child)) {
-        acc.push(child);
-      }
-      if (isValidElement(child) && child.props.children) {
-        acc.push(...filterRecursive(child.props.children, filterFn));
-      }
-      return acc;
+        if (filterFn(child)) {
+            acc.push(child);
+        }
+        if (isValidElement(child) && child.props.children) {
+            acc.push(...filterRecursive(child.props.children, filterFn));
+        }
+        return acc;
     }, []);
 }
